6500e99127c83e6329515df9c62def60b6a205d0
Jordy Parion <jj.pariona@uea.edu.ec>
2024-03-24 02:39:55 -0500

    DICCIONARIO S15

---
+++ SEMANA 15 DICCIONARIO.py
@@ -0,0 +1,30 @@
+# Definir mi diccionario
+persona = {
+    "nombre": "𝓐𝓷𝓪 𝓣𝓸𝓪𝓹𝓪𝓷𝓽𝓪",
+    "edad": 23,
+    "provincia": "𝓝𝓪𝓹𝓸",
+    "ciudad": "𝓔𝓵 𝓒𝓱𝓪𝓬𝓸",
+    "telefono": "0997569530",
+    "ocupacion": "𝓘𝓷𝓰𝓮𝓷𝓲𝓮𝓻𝓸"
+}
+
+# Acceder a los valores del diccionario
+print('►⚜️💻 ━━━━━━━━━━━━━━━━━━━━━━ 💻⚜️◀')
+print('         𝓜𝓲 𝓓𝓲𝓬𝓬𝓲𝓸𝓷𝓪𝓻𝓲́𝓸 ')
+print('►⚜️💻 ━━━━━━━━━━━━━━━━━━━━━━ 💻⚜️◀')
+print("       Nombre:", persona["nombre"])
+print("       Edad:", persona["edad"])
+print("       Provincia:", persona["provincia"])
+print("       Ciudad:", persona["ciudad"])
+print("       Teléfono:", persona["telefono"])
+print("       Ocupación:", persona["ocupacion"])
+
+
+# Agregar una nueva clave-valor al diccionario
+persona["email"] = "al.toapantat@uea.edu.ec"
+
+# Agregar una nueva clave-valor del diccionario
+del persona["telefono"]
+
+
+print(persona)
